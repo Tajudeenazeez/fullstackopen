adad4cd02b98e5409462ffe12295af4e27512a69
tajudeenazeez <azeezproto@gmail.com>
2022-12-10 23:01:50 +0100

    Ass1fso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
+import Button from "./component/Button";
+import Statistics from "./component/Statistics";
+import '../src/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  
+  const goodHandler = () => { setGood(good + 1)}
+  const badHandler = () => { setBad(bad + 1)}
+  const neutralHandler = () => { setNeutral(neutral + 1)}
+ 
+
+return (
+    <div>
+      <h1>give Feed back</h1>
+     <div className="wrap">
+      <Button onClick={goodHandler} text='good'/>
+      <Button onClick={badHandler} text='bad'/>
+      <Button onClick={neutralHandler} text='neutral'/>
+     
+     </div>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
+      {/* <h3>statistic</h3>
+      <p>good: {good}</p>
+      <p>bad: {bad}</p>
+      <p>neutral: {neutral}</p>
+      <p>average: {averageFeedback(good, bad, neutral)}</p>
+      <p>positive feedback: 0 </p> */}
     </div>
   );
-}
-
+};
 export default App;
