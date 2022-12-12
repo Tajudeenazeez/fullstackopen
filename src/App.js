328d77892d60b8b1b2176e283b83de3ab6938d9e
tajudeenazeez <azeezproto@gmail.com>
2022-12-12 08:34:53 +0100

    anecdotes part1d

--- src/App.js
+++ src/App.js
@@ -2,16 +2,48 @@ import React,{ useState } from "react";
 import Button from "./component/Button";
 import Statistics from "./component/Statistics";
 import '../src/App.css'
+import Anecdote from "./component/Anecdote";
+import { anecdotes, notes, courses} from "./data";
+import Vote from "./component/Vote";
+import List from "./component/List";
+// import Course from "./component/Course";
+
+
 
 
 const App = () => {
+  //feedback state
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  
+
+//feedback handler
   const goodHandler = () => { setGood(good + 1)}
   const badHandler = () => { setBad(bad + 1)}
   const neutralHandler = () => { setNeutral(neutral + 1)}
+
+  //anecdote creating room for vote dynamically, initial state as cache
+  const cache = {}
+  anecdotes.map((_, ind) => {
+    if(ind in cache ){
+      cache[ind] += 1
+    }
+    cache[ind] = 0
+    cache[ind] += 0 
+  })
+//anecdote state
+  let [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(cache)
+
+  //anecdote
+  const voteHandler = () => {setVote((prevState)=> {
+    const newVote = {...prevState}
+    newVote[selected] += 1
+    return {...newVote}
+  })}
+  const selectHandler = () => {
+    setSelected(Math.floor(Math.random()*anecdotes.length) )
+  }
  
 
 return (
@@ -24,12 +56,10 @@ return (
      
      </div>
       <Statistics good={good} bad={bad} neutral={neutral}/>
-      {/* <h3>statistic</h3>
-      <p>good: {good}</p>
-      <p>bad: {bad}</p>
-      <p>neutral: {neutral}</p>
-      <p>average: {averageFeedback(good, bad, neutral)}</p>
-      <p>positive feedback: 0 </p> */}
+      <Anecdote onClick={selectHandler} vote = {vote} selected={selected}/>
+      <Vote onClick={voteHandler} />
+      <List notes={notes}/>
+      {/* <Course courses={courses}/> */}
     </div>
   );
 };
